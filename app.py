36695a77da4f727fac74c1131f9fc05e9832ac2c
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2024-09-15 18:30:56 -0500

    Add code gateway with exception handling

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
 
+from gateway import \
+    ExceptionHandling
+
 app = Flask(__name__)
 
 app_context = app.app_context()
 app_context.push()
 
 cors = CORS(app)
+
+urlBaseReports_incidents = 'http://incidents:4001'
+
+@app.route('/report-incidents', methods=['POST','GET','PUT'])
+def reports_incidents():
+    return ExceptionHandling.communicate_to_microservice(ExceptionHandling, urlBaseReports_incidents + "/")
+    
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    return ExceptionHandling.get_message_not_found_url(ExceptionHandling)
 
 if __name__ == '__main__':
     app.run(port=4000)
--- gateway/gateway.py
+++ gateway/gateway.py
@@ -1,2 +1,50 @@
-from flask import request, jsonify
+from flask import request, json, jsonify
 import requests
+
+message_error = "Error to send request"
+
+class ExceptionHandling():
+
+    def get_message_not_found_url(self):
+        response = jsonify(self.get_response(404,"Resource not found, please contact with support"))
+        return response, 404
+
+    def get_response(status_code, message):
+        data_response = {
+            "message": message,
+            "status_code": status_code
+        }
+        return data_response
+
+    def communicate_to_microservice(self, endpoint):
+        try:
+            body = request.get_json()
+
+            response = requests.request(
+                method = request.method,
+                url = endpoint,
+                headers = request.headers,
+                data = request.get_data(),
+                params = request.args,
+                json = body,
+                timeout=110
+            )
+
+            status_code = response.status_code
+
+    
+            if status_code >= 400:
+                response.raise_for_status()
+            
+            return json.loads(response.content), response.status_code
+        
+        except requests.exceptions.Timeout as e:
+            status_code = 504
+            response = jsonify(self.get_response(status_code, message_error))
+            return response, status_code
+        
+        except requests.exceptions.RequestException as e:
+            status_code = getattr(e.response, 'status_code', 500)
+            response = jsonify(self.get_response(status_code, message_error))
+            return response, status_code
+        
